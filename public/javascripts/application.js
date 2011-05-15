713163a7a86a2ece9b011164754d2a256e2e5139
poramo <poramo@gmail.com>
2011-05-15 20:54:10 -0300

    SJN-Prioridad-FCFS andando.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,178 @@
+function RoundRobin(processes,quantum) {
+  var graph = [];
+  for (var i=0; i < processes.length; i++) {
+    graph[i] = [];
+  }
+
+  var cicle = 0;
+  var queue = new Queue();
+  var dif=0;
+  var bandera =false;
+
+  while (cantidadEliminados(processes) != processes.length) {
+    queue = RRNextProcess(cicle, processes, queue,bandera);
+
+    if (queue.isEmpty()==false) {
+      var index= queue.peek(); //saca el indice del frente
+      var dif= (processes[index].cicles - processes[index].countcicles);//aca hay un problema
+
+      if( dif > quantum){    //si los ciclos q le faltan - los q hizo son mayores q el quantum
+        var process=processes[index];
+        subtractRR(process, processes,quantum); // elimino los ciclos
+
+        for(var i=cicle ;i< (cicle + quantum);i++){
+          graph[index][i] = 'r';	}
+          for (var j=process.creationCicle; j<cicle;j++){
+            if(graph[process.index][j] != 'r')
+              graph[process.index][j]='w';
+          }
+
+          cicle+=quantum;
+          bandera=true;
+       }
+       else{
+         var process=processes[index];
+         processes[index].countcicles = processes[index].cicles;
+         queue.erase(index);
+         for(var i=cicle ;i< (cicle + dif);i++){
+           graph[index][i] = 'r';}
+         for (var j=process.creationCicle; j<cicle;j++){
+           if(graph[process.index][j] != 'r')
+             graph[process.index][j]='w';
+         }
+         cicle+=dif;
+         bandera=false;
+       }
+    }
+    else{
+     cicle++;
+    }
+  }
+
+  return graph;
+}
+
+
+function RRNextProcess(cicle, processes, queue,bandera) {
+
+  if(queue.tamanio != 0)
+    var pos=queue.tamanio();
+
+  for (var i=0; i < processes.length; i++) {
+    if ((cicle >= processes[i].creationCicle)&& (processes[i].cicles != processes[i].countcicles)) {
+
+      if (queue.pertenece(processes[i].index)==false) {
+        queue.enqueue(processes[i].index);
+      }
+    }
+  }
+
+//acomodar la ultima parte de la cola (esta parte falta)
+/*
+if(pos < queue.tamanio()){
+  for(var j=pos;j<queue.tamanio();j++){
+    menor=buscamenor(queue,processes,j);
+    queue.mover(menor,j);
+    alert("entra");
+  }}
+*/
+
+  if((bandera == true) && (queue.tamanio()>1)){
+    var ind=queue.peek();
+    queue.erase(ind);
+    queue.enqueue(ind);
+  }
+
+  return queue;
+}
+
+/*
+function buscamenor(queue,processes,posdesde){
+  var posMenor=posdesde;
+  var menor=processes[queue.mostrarpos(posdesde)].creationCicle;	
+  for(var k=posdesde+1;k<queue.tamanio();k++){
+    if(processes[queue.mostrarpos(k)].creationCicle < menor)
+    menor=queue.mostrarpos(k);
+    posMenor=k;
+  }
+  alert(menor);
+  return menor;
+}
+*/
+
+
+function subtractRR(process, processes,cant) {
+  for (var i=0; i < processes.length; i++) {
+    if (processes[i] == process) {
+      processes[i].countcicles+=cant;
+    }
+  }
+}
+
+function cantidadEliminados(processes){
+  var cantidad = 0;
+  for(var k=0 ;k < processes.length; k++){
+    if(processes[k].cicles == processes[k].countcicles)
+      cantidad++;
+  }
+  return cantidad;
+}
+
+//-------------------------Funcion Prioridad
+
+function prioridad(processes) {
+  var graph = [];
+  for (var i=0; i < processes.length; i++) {
+    graph[i] = [];
+  }
+
+  var cicle = 0;
+  // TODO en el futuro cambiar por haveBeenAllProcessesRan(processes)
+  while (processes.length != 0) {
+    var process = prioridadNextProcess(cicle, processes);
+    if (typeof process == 'object') {
+      graph[process.index][cicle] = 'r';
+
+      for (var k=process.creationCicle; k<cicle;k++){
+        if(graph[process.index][k] != 'r')
+          graph[process.index][k]='w';
+      }
+    subtractone(process, processes);
+    }
+    cicle++;
+  }
+  return graph;
+}
+
+function prioridadNextProcess(cicle, processes) {
+  var firstProcess = false;
+  for (var i=0; i < processes.length; i++) {
+    // TODO if (process[i].hasBeenRan()) continue;
+    if (cicle >= processes[i].creationCicle) {
+      if (firstProcess === false || processes[i].priority >firstProcess.priority  ) {
+        firstProcess = processes[i];
+      }
+    }
+  }
+  return firstProcess;
+}
+
+function subtractone(process, processes) {
+  for (var i=0; i < processes.length; i++) {
+    if (processes[i] == process) {
+      if (process.cicles == (process.countcicles + 1) ){
+        removeProcess(process,processes)
+        return true;
+      }
+      else{
+        processes[i].countcicles++;
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 function sjn(processes) {
   var graph = [];
   for (var i=0; i < processes.length; i++) {
@@ -265,26 +440,101 @@ function acceptNum(event){
   return ((key >= 48 && key <= 57) || ( key == 8)|| ( key == 75)  )
 }
 
-$(document).ready(function() {
-  $
-  // TODO Esta info debería salir de un formulario, asegurate de respetar los index.
-/*
-  var processes = [
-    { index: 0, creationCicle: 1, cicles: 2 },
-    { index: 1, creationCicle: 0, cicles: 2 },
-    { index: 2, creationCicle: 4, cicles: 3 },
-    { index: 3, creationCicle: 1, cicles: 1 },
+function valida(condicion) {
+
+  var quantum = document.getElementById('quantum').value*1;
+
+  var c0 = document.getElementById('llegada-01').value ;
+  var c1 = document.getElementById('llegada-02').value ;
+  var c2 = document.getElementById('llegada-03').value ;
+  var c3 = document.getElementById('llegada-04').value ;
+  var c4 = document.getElementById('llegada-05').value ;
+  var c5 = document.getElementById('llegada-06').value ;
+  var c6 = document.getElementById('llegada-07').value ;
+  var c7 = document.getElementById('llegada-08').value ;
+  var c8 = document.getElementById('llegada-09').value ;
+  var c9 = document.getElementById('llegada-10').value ;
+
+  var cy0 = document.getElementById('rafagas-01').value;
+  var cy1 = document.getElementById('rafagas-02').value;
+  var cy2 = document.getElementById('rafagas-03').value;
+  var cy3 = document.getElementById('rafagas-04').value;
+  var cy4 = document.getElementById('rafagas-05').value;
+  var cy5 = document.getElementById('rafagas-06').value;
+  var cy6 = document.getElementById('rafagas-07').value;
+  var cy7 = document.getElementById('rafagas-08').value;
+  var cy8 = document.getElementById('rafagas-09').value;
+  var cy9 = document.getElementById('rafagas-10').value;
+
+  var p0 = document.getElementById('prioridad-01').value;
+  var p1 = document.getElementById('prioridad-02').value;
+  var p2 = document.getElementById('prioridad-03').value;
+  var p3 = document.getElementById('prioridad-04').value;
+  var p4 = document.getElementById('prioridad-05').value;
+  var p5 = document.getElementById('prioridad-06').value;
+  var p6 = document.getElementById('prioridad-07').value;
+  var p7 = document.getElementById('prioridad-08').value;
+  var p8 = document.getElementById('prioridad-09').value;
+  var p9 = document.getElementById('prioridad-10').value;
+
+// TODO validar si estan en blanco
+
+  var processe = [
+    { index: 0, creationCicle: c0, cicles: cy0, priority: p0, countcicles: 0 },
+    { index: 1, creationCicle: c1, cicles: cy1, priority: p1, countcicles: 0 },
+    { index: 2, creationCicle: c2, cicles: cy2, priority: p2, countcicles: 0 },
+    { index: 3, creationCicle: c3, cicles: cy3, priority: p3, countcicles: 0 },
+    { index: 4, creationCicle: c4, cicles: cy4, priority: p4, countcicles: 0 },
+    { index: 5, creationCicle: c5, cicles: cy5, priority: p5, countcicles: 0 },
+    { index: 6, creationCicle: c6, cicles: cy6, priority: p6, countcicles: 0 },
+    { index: 7, creationCicle: c7, cicles: cy7, priority: p7, countcicles: 0 },
+    { index: 8, creationCicle: c8, cicles: cy8, priority: p8, countcicles: 0 },
+    { index: 9, creationCicle: c9, cicles: cy9, priority: p9, countcicles: 0 }
   ];
 
-  var processes_table = [
-    { index: 0, creationCicle: 1, cicles: 2 },
-    { index: 1, creationCicle: 0, cicles: 2 },
-    { index: 2, creationCicle: 4, cicles: 3 },
-    { index: 3, creationCicle: 1, cicles: 1 },
-  ];
+  var z = 0;
+  var processes = [];
 
-  drawGraph(new GraphData(fifo(processes)));
+  for (var i=0; i < 10; i++) {
+    if ( (processe[i].creationCicle == '') || (processe[i].cicles == '') || (processe[i].priority == '')){
+      // something
+    }
+    else{
+      processes.push({
+         index       : z, creationCicle : processe[i].creationCicle * 1,
+         cicles      : processe[i].cicles *1,
+         priority    : processe[i].priority *1,
+         countcicles : 0
+       });
 
-  drawTable(new GraphData(fifo(processes_table)));
-*/
-});
+       z++;
+     }
+  }
+
+  switch (condicion) {
+    case 'sjn':
+      grafico = new GraphData(sjn(processes));
+      drawGraph(grafico);
+      drawTable(grafico);
+      break
+
+    case 'fifo':
+      grafico = new GraphData(fifo(processes));
+      drawGraph(grafico);
+      drawTable(grafico);
+      break
+
+    case 'prioridad':
+      grafico = new GraphData(prioridad(processes));
+      drawGraph(grafico);
+      drawTable(grafico);
+      break
+
+    case 'RR':
+      alert(condicion);
+      grafico = new GraphData(RoundRobin(processes,quantum));
+      drawGraph(grafico);
+      drawTable(grafico);
+      break
+  }
+}
